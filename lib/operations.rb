7df4b7260b8331e2f5d0e9feff6716a7d2fdc90e
Bryan McAfee <brymcafee@gmail.com>
2018-04-24 15:48:51 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,13 @@
 def unsafe?(speed)
-unsafe= speed < 40 || speed > 60
+if speed > 60
+	true
+elsif speed < 40
+	true
+else
+	false
+end
 end
 
 def not_safe?(speed)
 speed= < 40 || > 60 ? true : false
 end
-	
